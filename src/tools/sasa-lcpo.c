2c202efff55d0591419c975107becbb813c50f4d
jdqing <2005jdq@gmail.com>
2016-12-27 11:54:02 +0800

    add sasa-setup

--- src/tools/sasa-lcpo.c
+++ src/tools/sasa-lcpo.c
@@ -2,16 +2,259 @@
  *
  */
 
-double *gbsafrc;
+double **gbsafrc;
 double *vdwrad;
 double *lcpo_p1;
 double *lcpo_p2;
 double *lcpo_p3;
 double *lcpo_p4;
 
-int *ineighbor;
+int **ineighbor;
 int *j_vals;
 int *k_vals;
+
+int sasa_setup(int atm_cnt, int *num_ints, int *num_reals)
+{
+  int alloc_failed;
+  int *bond_partner;
+  int *total_bond_partner;
+
+  int i;
+  int atm_1;
+  int atm_2;
+  int num_bonds;
+  int total_bonds;
+  int atomicnumber;
+  char isymbl[4]; //holder for atom type
+
+  //num_ints and num_reals are used to return allocation counts. Don't zero.
+
+  //ROMEE Add this before calling  if (gbsa == 1) then
+
+  //First we calloc the main arrays
+
+  gbsafrc = (double **)calloc((atm_cnt+1) * sizeof(double *));
+  for(i=1;i<=atm_cnt;i++)
+  {
+    *(gbsafrc + i) = (double *)calloc(3 * sizeof(double));
+  }
+
+  vdwrad  = (double *)calloc((atm_cnt+1) * sizeof(double));
+  lcpo_p1 = (double *)calloc((atm_cnt+1) * sizeof(double));
+  lcpo_p2 = (double *)calloc((atm_cnt+1) * sizeof(double));
+  lcpo_p3 = (double *)calloc((atm_cnt+1) * sizeof(double));
+  lcpo_p4 = (double *)calloc((atm_cnt+1) * sizeof(double));
+
+  ineighbor = (int **)calloc((atm_cnt+1) * sizeof(int *));
+  for(i=1;i<=atm_cnt;i++)
+  {
+    *(ineighbor + i) = (int *)calloc(40 * sizeof(int));
+  }
+
+  j_vals = (int *)calloc((atm_cnt+1) * sizeof(int));
+  k_vals = (int *)calloc((atm_cnt+1) * sizeof(int));
+  bond_partner = (int *)calloc((atm_cnt+1) * sizeof(int));
+  total_bond_partner = (int *)calloc((atm_cnt+1) * sizeof(int));
+
+  /*
+  num_reals = num_reals + size(lcpo_p1) + &
+                          size(lcpo_p2) + &
+                          size(lcpo_p3) + &
+                          size(lcpo_p4) + &
+                          size(vdwrad)*/
+  num_reals = num_reals + 5 * atm_cnt;
+
+  num_ints = num_ints + (1 + 1 + 40 + 1 + 1) * atm_cnt;
+
+  //For each atom index we fing in the BONDS_WITHOUT_HYDROGEN array, add
+  //1 to that index of bond_partner.taht way,we can tell how many atoms
+  //are bonded to each atom
+
+  for(i = 0; i <= nbona -1; i++)
+  {
+    atm_1 = gbl_bond[bonda_idx + i]%atm_i;
+    atm_2 = gbl_bond[bonda_idx + i]%atm_j;
+    bond_partner[atm_1] = bond_partner[atm_1] + 1;
+    bond_partner[atm_2] = bond_partner[atm_2] + 1;
+  }
+
+  total_bond_partner = bond_partner;
+
+  for(i = 1; i <= nbonh; i++)
+  {
+    atm_1 = gbl_bond[i]%atm_i;
+    atm_2 = gbl_bond[i]%atm_j;
+    total_bond_partner[atm_1] = total_bond_partner[atm_1] + 1;
+    total_bond_partner[atm_2] = total_bond_partner[atm_2] + 1;
+  }
+
+  //Construct the parameters for the SA calculation; note taht the radii
+  //stored in vdwrad are augmented by 1.4 Angstroms
+
+  for(i = 1; i <= atm_cnt; i++)
+  {
+    if(loaded_atm_atomicnumber)
+      atomicnumber = atm_atomicnumber[i];
+    else
+      call get_atomic_number(atm_igraph(i), atm_mass(i), atomicnumber);
+
+    isymbl = atm_isymbl[i];
+    call upper(isymbl);
+    num_bonds = bond_partner[i];
+    total_bond_partner = total_bond_partner[i];
+
+    if(atomicnumber == 6)
+    {
+      if(total_bonds == 4)
+      {
+        if(num_bonds == 1)
+        {
+          vdwrad(i)  = 1.70d0 + 1.4d0
+          lcpo_p1(i) = 0.77887d0
+          lcpo_p2(i) = -0.28063d0
+          lcpo_p3(i) = -0.0012968d0
+          lcpo_p4(i) = 0.00039328d0
+        }
+        else if(num_bonds == 2)
+        {
+          vdwrad(i)  = 1.70d0 + 1.4d0
+          lcpo_p1(i) = 0.56482d0
+          lcpo_p2(i) = -0.19608d0
+          lcpo_p3(i) = -0.0010219d0
+          lcpo_p4(i) = 0.0002658d0
+        }
+        else if(num_bonds == 3)
+        {
+          vdwrad(i)  = 1.70d0 + 1.4d0
+          lcpo_p1(i) = 0.23348d0
+          lcpo_p2(i) = -0.072627d0
+          lcpo_p3(i) = -0.00020079d0
+          lcpo_p4(i) = 0.00007967d0
+        }
+        else if(num_bonds == 4)
+        {
+          vdwrad(i)  = 1.70d0 + 1.4d0
+          lcpo_p1(i) = 0.00000d0
+          lcpo_p2(i) = 0.00000d0
+          lcpo_p3(i) = 0.00000d0
+          lcpo_p4(i) = 0.00000d0
+        }
+        else
+        {
+          write(6,*) 'Unusual nbond for sp3 C:', i, num_bonds, &
+            ' Using default carbon LCPO parameters'
+          vdwrad(i)  = 1.70d0 + 1.4d0
+          lcpo_p1(i) = 0.77887d0
+          lcpo_p2(i) = -0.28063d0
+          lcpo_p3(i) = -0.0012968d0
+          lcpo_p4(i) = 0.00039328d0
+        }
+      }
+      else
+      {
+        if(num_bonds == 2)
+        {
+          vdwrad(i)  = 1.70d0 + 1.4d0
+          lcpo_p1(i) = 0.51245d0
+          lcpo_p2(i) = -0.15966d0
+          lcpo_p3(i) = -0.00019781d0
+          lcpo_p4(i) = 0.00016392d0
+        }
+        else if(num_bonds == 3)
+        {
+          vdwrad(i)  = 1.70d0 + 1.4d0
+          lcpo_p1(i) = 0.070344d0
+          lcpo_p2(i) = -0.019015d0
+          lcpo_p3(i) = -0.000022009d0
+          lcpo_p4(i) = 0.000016875d0
+        }
+        else
+        {
+          write(6,*) 'Unusual nbond for C :', i, num_bonds, &
+            ' Using default carbon LCPO parameters'
+          vdwrad(i)  = 1.70d0 + 1.4d0
+          lcpo_p1(i) = 0.77887d0
+          lcpo_p2(i) = -0.28063d0
+          lcpo_p3(i) = -0.0012968d0
+          lcpo_p4(i) = 0.00039328d0
+        }
+      }
+    }
+    else if(atomicnumber == 8)
+    {
+      if(isymbl[]=="O")
+      {
+        vdwrad(i)  = 1.60d0 + 1.4d0
+        lcpo_p1(i) = 0.68563d0
+        lcpo_p2(i) = -0.1868d0
+        lcpo_p3(i) = -0.00135573d0
+        lcpo_p4(i) = 0.00023743d0
+      }
+      else if(isymbl[]=="O2")
+      {
+        vdwrad(i)  = 1.60d0 + 1.4d0
+        lcpo_p1(i) = 0.88857d0
+        lcpo_p2(i) = -0.33421d0
+        lcpo_p3(i) = -0.0018683d0
+        lcpo_p4(i) = 0.00049372d0
+      }
+      else
+      {
+        if(num_bonds == 1)
+        {
+          vdwrad(i)  = 1.60d0 + 1.4d0
+          lcpo_p1(i) = 0.77914d0
+          lcpo_p2(i) = -0.25262d0
+          lcpo_p3(i) = -0.0016056d0
+          lcpo_p4(i) = 0.00035071d0
+        }
+        else if(num_bonds == 2)
+        {
+          vdwrad(i)  = 1.60d0 + 1.4d0
+          lcpo_p1(i) = 0.49392d0
+          lcpo_p2(i) = -0.16038d0
+          lcpo_p3(i) = -0.00015512d0
+          lcpo_p4(i) = 0.00016453d0
+        }
+        else
+        {
+          write(6,*) 'Unusual nbond for O:', i, num_bonds, &
+             ' Using default oxygen LCPO parameters'
+          vdwrad(i)  = 1.60d0 + 1.4d0
+          lcpo_p1(i) = 0.77914d0
+          lcpo_p2(i) = -0.25262d0
+          lcpo_p3(i) = -0.0016056d0
+          lcpo_p4(i) = 0.00035071d0
+        }
+      }
+    }
+    else if(atomicnumber == 7)
+    {
+      if(isymbl == "N3")
+      {
+        if(num_bonds == 1)
+        {
+          vdwrad(i)  = 1.65d0 + 1.4d0
+          lcpo_p1(i) = 0.078602d0
+          lcpo_p2(i) = -0.29198d0
+          lcpo_p3(i) = -0.0006537d0
+          lcpo_p4(i) = 0.00036247d0
+        }
+        else if(num_bonds == 2)
+        {
+          vdwrad(i)  = 1.65d0 + 1.4d0
+          lcpo_p1(i) = 0.22599d0
+          lcpo_p2(i) = -0.036648d0
+          lcpo_p3(i) = -0.0012297d0
+          lcpo_p4(i) = 0.000080038d0
+        }
+        else if(num_bonds == 3)
+      }
+    }
+  }
+
+
+}
 
 int sasa_lcpo(double *crd, double *frc, int max_count,
               int max_i, double frespa, double *esurf)
